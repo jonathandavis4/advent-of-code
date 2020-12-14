fe4e28b596068dd41a81bfce04a59a5c9511e10d
Jonathan Davis <jonathandavis47780@gmail.com>
2020-12-14 21:48:15 +0000

    Add solutions for 2020 day 1.

---
+++ 2020/day_1/part_1.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using std::cout, std::ifstream, std::vector;
+
+vector<int> get_input() {
+	ifstream input_stream ("input.txt");
+	vector<int> numbers;
+	int number;
+	while(! input_stream.eof()) {
+		input_stream >> number;
+		numbers.push_back(number);
+	}
+	input_stream.close();
+	return numbers;
+}
+
+int main() {
+	vector<int> numbers = get_input();
+
+	for (int number_1: numbers) {
+		for (int number_2: numbers) {
+			if (number_1 + number_2 == 2020) {
+				cout << number_1 * number_2 << "\n";
+				return 0;
+			}
+		}
+	}
+	
+	return 0;
+}
---
+++ 2020/day_1/part_2.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using std::cout, std::ifstream, std::vector;
+
+vector<int> get_input() {
+	ifstream input_stream ("input.txt");
+	vector<int> numbers;
+	int number;
+	while(! input_stream.eof()) {
+		input_stream >> number;
+		numbers.push_back(number);
+	}
+	input_stream.close();
+	return numbers;
+}
+
+int main() {
+	vector<int> numbers = get_input();
+
+	for (int number_1: numbers) {
+		for (int number_2: numbers) {
+			for (int number_3: numbers) {
+				if (number_1 + number_2 + number_3 == 2020) {
+					cout << number_1 * number_2 * number_3 << "\n";
+					return 0;
+				}
+			}
+		}
+	}
+	
+	return 0;
+}
